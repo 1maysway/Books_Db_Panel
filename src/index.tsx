3a125d5fe325ab2f1d6b3a051fcb7e5556eb201f
maysway <1maysway@gmail.com>
2023-03-02 13:47:23 +0400

    added the basename to browser router in index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/Books_Db_Panel'>
     <App />
   </BrowserRouter>
 );
